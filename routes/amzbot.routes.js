e0a6a759d109bac3d6ff6109734a24e598bf0995
nikitaartemev <nikitaart2805@gmail.com>
2021-02-12 16:11:57 -0800

    New v

--- routes/amzbot.routes.js
+++ routes/amzbot.routes.js
@@ -12,24 +12,24 @@ let OfferStatus ="Not searching"
 let interval ;
 
 
-io.on("connection", (socket) => {
-    console.log("New client connected");
-    if (interval) {
-        clearInterval(interval);
-    }
-
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-    });
-});
-
-const getApiAndEmit = socket => {
-    const response = new Date();
-    // Emitting a new message. Will be consumed by the client
-    socket.emit("FromAPI", response);
-};
+// io.on("connection", (socket) => {
+//     console.log("New client connected");
+//     if (interval) {
+//         clearInterval(interval);
+//     }
+//
+//     interval = setInterval(() => getApiAndEmit(socket), 1000);
+//     socket.on("disconnect", () => {
+//         console.log("Client disconnected");
+//         clearInterval(interval);
+//     });
+// });
+//
+// const getApiAndEmit = socket => {
+//     const response = new Date();
+//     // Emitting a new message. Will be consumed by the client
+//     socket.emit("FromAPI", response);
+// };
 
 
 
